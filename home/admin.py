5767acb1cb27c8d52575b2b6800a1bd55e5ea4a2
Crowdbotics <team@crowdbotics.com>
2022-02-10 09:59:32 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Mandapform
+
+admin.site.register(Mandapform)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,12 @@
+from django.conf import settings
 from django.db import models
+
+
+class Mandapform(models.Model):
+    "Generated Model"
+    category = models.CharField(
+        max_length=256,
+    )
+    type = models.CharField(
+        max_length=256,
+    )
